bcc5946c8f0f4ce3bbdc0f33c08251ffe5727f56
razmuz99 <razmuz99@gmail.com>
2021-11-09 16:22:11 +0100

    Login activity

--- app/src/main/java/com/example/vmau_quizapp/MainActivity.java
+++ app/src/main/java/com/example/vmau_quizapp/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.vmau_quizapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
